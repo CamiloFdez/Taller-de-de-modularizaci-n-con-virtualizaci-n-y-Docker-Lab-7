afdb03f3d610dda455bd41c2664cf66b067c5d61
CamiloFdez <camilo.fernandez-d@mail.escuelaing.edu.co>
2026-03-12 22:51:20 -0500

    Adding what we did in lab

---
+++ src/test/java/edu/eci/dockerLab/DockerLabApplicationTests.java
@@ -0,0 +1,8 @@
+package edu.eci.dockerLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerLabApplicationTests {
+}
